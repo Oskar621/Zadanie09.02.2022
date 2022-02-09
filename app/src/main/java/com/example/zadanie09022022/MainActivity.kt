9454e15e0ee60ff315fd2786836199d6f9eef698
Oskar621 <o_swiderski@o2.pl>
2022-02-09 08:30:01 +0100

    Koniec zadania

--- app/src/main/java/com/example/zadanie09022022/MainActivity.kt
+++ app/src/main/java/com/example/zadanie09022022/MainActivity.kt
@@ -13,14 +13,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setupAutoCompleteTextView()
     }
+
     private fun setupAutoCompleteTextView() {
         val countries = resources.getStringArray(R.array.countries_array)
-        // Create the adapter and set it to the AutoCompleteTextView
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
         binding.autoCompleteTextView.setAdapter(adapter)
 
-        binding.wyslij.setOnClickListener {
+        binding.btnSubmit.setOnClickListener {
             val text = getString(R.string.entered_text) + " " + binding.autoCompleteTextView.text
             Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
         }
